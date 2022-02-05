f31c6ae53f9ca4ea71644d5e1e2830b9decda866
Фараджов Теймур <only4youmother@gmail.com>
2022-02-06 04:36:39 +0600

    Добавил NavMesh2D, АИ стал немного умнее

---
+++ TDS/Assets/Scripts/ShowDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDamage : MonoBehaviour
+{
+    [HideInInspector] public float damage;
+    private TextMesh textMesh;
+    void Start()
+    {
+     textMesh = GetComponent<TextMesh>();
+     textMesh.text = "-" + damage;
+    }
+
+    public void OnAnimationOver()
+    {
+        Destroy(gameObject);
+    }
+
+}
